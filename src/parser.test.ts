40c5876aa0ed208c334517a8f7cdb861d8d93bcf
maciejziemichod <maciej.ziemichod@gmail.com>
2023-10-24 17:46:32 +0200

    test: return statements

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,4 +1,4 @@
-import { LetStatement } from "./ast";
+import { LetStatement, ReturnStatement } from "./ast";
 import { Lexer } from "./lexer";
 import { Parser } from "./parser";
 
@@ -14,17 +14,7 @@ let foobar = 838383;
 
     const program = parser.parseProgram();
 
-    const errors = parser.getErrors();
-
-    try {
-        expect(errors.length).toBe(0);
-    } catch (_) {
-        throw new Error(
-            `Expected 0 errors during parsing, got ${
-                errors.length
-            } instead:\n${errors.join("\n")}`,
-        );
-    }
+    checkParserErrors(parser);
 
     expect(program).toBeDefined();
     expect(program.statements.length).toBe(3);
@@ -48,3 +38,42 @@ let foobar = 838383;
         expect(statement.name.getTokenLiteral()).toBe(expectedIdentifier);
     });
 });
+
+test("test ReturnStatement", () => {
+    const input = `
+return 5;
+return 10;
+return 993322;
+`;
+    const lexer = new Lexer(input);
+    const parser = new Parser(lexer);
+
+    const program = parser.parseProgram();
+
+    checkParserErrors(parser);
+
+    expect(program).toBeDefined();
+    expect(program.statements.length).toBe(3);
+
+    program.statements.forEach((statement) => {
+        expect(statement.getTokenLiteral()).toBe("return");
+
+        const isReturnStatement = statement instanceof ReturnStatement;
+
+        expect(isReturnStatement).toBe(true);
+    });
+});
+
+function checkParserErrors(parser: Parser): void {
+    const errors = parser.getErrors();
+
+    try {
+        expect(errors.length).toBe(0);
+    } catch (_) {
+        throw new Error(
+            `Expected 0 errors during parsing, got ${
+                errors.length
+            } instead:\n${errors.join("\n")}`,
+        );
+    }
+}
