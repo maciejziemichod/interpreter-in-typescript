e16c6062b89167e58ce01659d263324c01d409da
maciejziemichod <maciej.ziemichod@gmail.com>
2023-10-25 00:53:19 +0200

    feat: partial parsing expressions

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,6 @@
 import {
+    Expression,
+    ExpressionStatement,
     Identifier,
     LetStatement,
     Program,
@@ -8,14 +10,38 @@ import {
 import { Lexer } from "./lexer";
 import { Token, TokenItem, TokenType } from "./token";
 
+type prefixParseFunction = () => Expression;
+type infixParseFunction = (expression: Expression) => Expression;
+
+const Precendence = {
+    LOWEST: 1,
+    EQUALS: 2, // ==
+    LESS_GREATER: 3, // > or <
+    SUM: 4, // +
+    PRODUCT: 5, // *
+    PREFIX: 6, // -X or !X
+    CALL: 7, // myFunction(X)
+} as const;
+type PrecendenceValue = (typeof Precendence)[keyof typeof Precendence];
+
 export class Parser {
     private lexer: Lexer;
     private currentToken: Token;
     private peekToken: Token;
     private errors: string[] = [];
+    private prefixParseFunctions: Partial<{
+        [key in TokenItem]: prefixParseFunction;
+    }> = {};
+    private infixParseFunctions: Partial<{
+        [key in TokenItem]: infixParseFunction;
+    }> = {};
 
     constructor(lexer: Lexer) {
         this.lexer = lexer;
+
+        this.parseIdentifier = this.parseIdentifier.bind(this);
+
+        this.registerPrefix(TokenType.IDENTIFIER, this.parseIdentifier);
 
         this.nextToken();
         this.nextToken();
@@ -60,7 +86,7 @@ export class Parser {
             case TokenType.RETURN:
                 return this.parseReturnStatement();
             default:
-                return null;
+                return this.parseExpressionStatement();
         }
     }
 
@@ -103,6 +129,38 @@ export class Parser {
         return statement;
     }
 
+    private parseExpressionStatement(): ExpressionStatement | null {
+        const statement = new ExpressionStatement();
+        statement.token = this.currentToken;
+
+        const expression = this.parseExpression(Precendence.LOWEST);
+
+        // TODO or maybe make expression field allow null
+        if (expression === null) {
+            return null;
+        }
+
+        statement.expression = expression;
+
+        if (this.peekTokenIs(TokenType.SEMICOLON)) {
+            this.nextToken();
+        }
+
+        return statement;
+    }
+
+    private parseExpression(precendence: PrecendenceValue): Expression | null {
+        const prefix = this.prefixParseFunctions[this.currentToken.type];
+
+        if (prefix === undefined) {
+            return null;
+        }
+
+        const leftExpression = prefix();
+
+        return leftExpression;
+    }
+
     private currentTokenIs(type: TokenItem): boolean {
         return this.currentToken.type === type;
     }
@@ -119,5 +177,24 @@ export class Parser {
             this.peekError(type);
             return false;
         }
+    }
+
+    private registerPrefix(
+        tokenType: TokenItem,
+        fn: prefixParseFunction,
+    ): void {
+        this.prefixParseFunctions[tokenType] = fn;
+    }
+
+    private registerInfix(tokenType: TokenItem, fn: infixParseFunction): void {
+        this.infixParseFunctions[tokenType] = fn;
+    }
+
+    private parseIdentifier(): Expression {
+        const identifier = new Identifier();
+        identifier.token = this.currentToken;
+        identifier.value = this.currentToken.literal;
+
+        return identifier;
     }
 }
