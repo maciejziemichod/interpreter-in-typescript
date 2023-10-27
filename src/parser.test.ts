817a1626715fcc41894397c69d861e2924dfb239
maciejziemichod <maciej.ziemichod@gmail.com>
2023-10-27 23:26:46 +0200

    test: grouped expressions

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -245,6 +245,11 @@ test("test operator precedence parsing", () => {
         ["false", "false"],
         ["3 > 5 == false", "((3 > 5) == false)"],
         ["3 < 5 == true", "((3 < 5) == true)"],
+        ["1 + (2 + 3) + 4", "((1 + (2 + 3)) + 4)"],
+        ["(5 + 5) * 2", "((5 + 5) * 2)"],
+        ["2 / (5 + 5)", "(2 / (5 + 5))"],
+        ["-(5 + 5)", "(-(5 + 5))"],
+        ["!(true == true)", "(!(true == true))"],
     ];
 
     tests.forEach(([input, expected]) => {
