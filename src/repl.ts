02cea3b849e45ce6ca8930850e99cd9690a1f61c
maciejziemichod <maciej.ziemichod@gmail.com>
2023-10-28 17:45:01 +0200

    feat: improve REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,6 @@
 import readline from "readline";
 import { Lexer } from "./lexer";
+import { Parser } from "./parser";
 import { TokenType } from "./token";
 
 export function startRepl(
@@ -16,15 +17,40 @@ export function startRepl(
 
     rl.on("line", (line) => {
         const lexer = new Lexer(line);
+        const parser = new Parser(lexer);
 
-        for (
-            let token = lexer.getNextToken();
-            token.type !== TokenType.EOF;
-            token = lexer.getNextToken()
-        ) {
-            console.log(token);
+        const program = parser.parseProgram();
+
+        if (parser.getErrors().length !== 0) {
+            printParserErrors(parser.getErrors());
+
+            return;
         }
 
+        console.log(program.string());
+
         rl.prompt();
+    });
+}
+
+function printParserErrors(errors: string[]): void {
+    console.log(`
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \\/  .-. .-.  \\/ .. \\
+ | |  '|  /   Y   \\  |'  | |
+ | \\   \\  \\ 0 | 0 /  /   / |
+  \\ '- ,\\.-"'' ''"-./, -' /
+   ''-' /_   ^ ^   _\\ '-''
+       |  \\._   _./  |
+       \\   \\ '~' /   /
+        '._ '-=-' _.'
+           '~---~'
+`);
+    console.log("Woops! We ran into some monkey business here!");
+    console.log(" parser errors:");
+
+    errors.forEach((error) => {
+        console.log(`\t${error}\n`);
     });
 }
